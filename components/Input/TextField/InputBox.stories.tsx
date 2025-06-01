34b5c954078e9c490f6e930ea2db55fe733a6cab
seojin-kim12 <80455266+seojin-kim12@users.noreply.github.com>
2025-06-01 18:35:45 +0900

    feat: input/textField components add (#6)

---
+++ components/Input/TextField/InputBox.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { InputBox } from './InputBox';
+
+const meta: Meta<typeof InputBox> = {
+  title: 'Components/InputBox',
+  component: InputBox,
+  tags: ['autodocs'],
+  args: {
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof InputBox>;
+
+export const Default: Story = {
+  args: {
+    provider: 'default',
+  },
+};
+
+export const Active: Story = {
+  args: {
+    provider: 'active',
+  },
+};

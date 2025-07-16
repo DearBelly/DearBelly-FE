dd46fa8e1fbf86bd6c2653971740d203bf18daf2
Jiyeon <144954836+Chaejy@users.noreply.github.com>
2025-07-16 13:51:43 +0900

    [FEATURE] 버튼 컴포넌트 구현 (#31)
    * feat: 버튼 컴포넌트 제작
    
    * feat: pros에서 height 제거

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: { action: 'clicked' },
+    width: { control: 'text' },
+    size: {
+      control: { type: 'radio' },
+      options: ['large', 'medium', 'small'],
+    },
+    type: {
+      control: { type: 'radio' },
+      options: ['primary', 'secondary'],
+    },
+    disabled: { control: 'boolean' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const PrimaryLarge: Story = {
+  args: {
+    type: 'primary',
+    size: 'large',
+    children: 'primary large',
+  },
+};
+
+export const SecondaryMedium: Story = {
+  args: {
+    type: 'secondary',
+    size: 'medium',
+    children: 'secondary medium',
+  },
+};
+
+export const DisabledSmall: Story = {
+  args: {
+    type: 'primary',
+    size: 'small',
+    disabled: true,
+    children: '비활성',
+  },
+};
+
+export const CustomSize: Story = {
+  args: {
+    type: 'primary',
+    size: 'medium',
+    width: '10rem',
+    children: '커스텀 크기',
+  },
+};

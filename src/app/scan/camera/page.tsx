145e45306bfd1e013ec2752c90d69353cf016333
seojin-kim12 <80455266+seojin-kim12@users.noreply.github.com>
2025-08-17 03:54:07 +0900

    [Feature] 컴퓨터 비전 기능 구현 1차 완료 (#54)

--- src/app/scan/camera/page.tsx
+++ src/app/scan/camera/page.tsx
@@ -6,11 +6,16 @@ import { useGetBreakPointValue } from '../../../context/BreakPointProvider';
 import { PhotoGuideModal } from '../../../components/ComputerVision/Photo/PhotoGuideModal';
 import { PhotoBtn } from '../../../components/ComputerVision/Photo/PhotoBtn';
 import { useRouter } from 'next/navigation';
+import { X } from "@mynaui/icons-react";
 
 export default function Camera() {
   const isPc = useGetBreakPointValue();
   const isMobile = !isPc;
   const router = useRouter();
+  
+  const handleBackClick = () => {
+    router.push('/scan');
+  };
 
   const handleImageUpload = (file: File) => {
     console.log('촬영된 이미지:', file);
@@ -28,6 +33,16 @@ export default function Camera() {
 
   const content = (
     <Box bg="#737373" minH="100vh" display="flex" alignItems="center" justifyContent="center">
+      <Box 
+        position='fixed'
+        top='1.25rem'
+        right='1.25rem' 
+        zIndex='1000'
+        display='flex'
+        cursor='pointer'
+      > 
+        <X size='1.5rem' color='white' strokeWidth={1.5} onClick={handleBackClick}/> 
+      </Box>
       <PhotoGuideModal
         // 모바일이면 후면 카메라 우선
         source="camera"                 
@@ -35,6 +50,15 @@ export default function Camera() {
         onImageUpload={handleImageUpload}
          // confirm 버튼 누르면 여기로 데이터 URL이 옴
         onCrop={handleCrop}           
+        initialImage="/images/med.svg" 
+        title="의약품 촬영 가이드"     
+        content={
+          <>
+            단일 알약만 인식됩니다<br/>
+            알약 표면의 글자가 또렷하게 보이도록<br/>
+            가이드라인 안에 알약 하나만 맞춰주세요<br/>
+          </>
+        }
       >
         {/* 선택된 이미지가 없을 때 아래의 버튼만 보임 */}
         <PhotoBtn variant="large" data-role="take">
--- src/app/scan/gallery/page.tsx
+++ src/app/scan/gallery/page.tsx
@@ -6,11 +6,16 @@ import { useGetBreakPointValue } from '../../../context/BreakPointProvider';
 import { PhotoGuideModal } from '../../../components/ComputerVision/Photo/PhotoGuideModal';
 import { PhotoBtn } from '../../../components/ComputerVision/Photo/PhotoBtn';
 import { useRouter } from 'next/navigation';
+import { X } from "@mynaui/icons-react";
 
 export default function Gallery() {
   const isPc = useGetBreakPointValue();
   const isMobile = !isPc;
   const router = useRouter();
+
+  const handleBackClick = () => {
+    router.push('/scan');
+  };
 
   const handleImageUpload = (file: File) => {
     console.log('촬영된 이미지:', file);
@@ -28,11 +33,29 @@ export default function Gallery() {
 
   const content = (
     <Box bg="#737373" minH="100vh" display="flex" alignItems="center" justifyContent="center">
+      <Box 
+        position='fixed'
+        top='1.25rem'
+        right='1.25rem' 
+        zIndex='1000'
+        display='flex'
+        cursor='pointer'
+      > 
+        <X size='1.5rem' color='white' strokeWidth={1.5} onClick={handleBackClick}/> 
+      </Box>
       <PhotoGuideModal
         source="gallery"
         accept="image/*"
         onImageUpload={handleImageUpload}
         onCrop={handleCrop}
+        initialImage="/images/med.svg" 
+        title="의약품 촬영 가이드"     
+        content={
+          <>
+            단일 알약만 인식됩니다<br/>
+            가이드라인 안에 알약 하나만 맞춰주세요
+          </>
+        }
       >
         <PhotoBtn variant="large" data-role="take">앨범에서 업로드하기</PhotoBtn>
         <PhotoBtn variant="assistive" data-role="retake">다시 업로드하기</PhotoBtn>

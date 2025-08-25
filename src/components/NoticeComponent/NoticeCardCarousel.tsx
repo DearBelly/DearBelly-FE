4ba1a4aece569e573eb4e429705d15864853d2eb
seojin-kim12 <rlatjwls10403@naver.com>
2025-08-26 00:03:13 +0900

    chore: info page notice card ui error edit

--- src/components/NoticeComponent/NoticeCardCarousel.tsx
+++ src/components/NoticeComponent/NoticeCardCarousel.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import Slider from "react-slick";
+// 이거 없애면 안 됩니다!!!!
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Box } from "@chakra-ui/react";
 import { NoticeBox } from "./NoticeBox";
 import type { NoticeBoxProps } from "./NoticeBox";

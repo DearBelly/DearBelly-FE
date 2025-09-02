1c633d9d39f4b7c6f31acd37ebca904f372e3b58
Chaejy <xhdrp@sookmyung.ac.kr>
2025-09-02 13:34:28 +0900

    feature: 편지함 편지확인 및 수정

---
+++ src/app/letters/[date]/me/edit/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { TopBarBottomButtonLayout } from "@/components/Layouts/TopBarBottomButtonLayout";
+import { LetterEditBox } from "@/components/TextField/LetterEditBox";
+import { Box } from "@chakra-ui/react";
+import { TrashSolid } from "@mynaui/icons-react";
+import { useRouter } from "next/navigation";
+
+export default function EditLetterPage({
+  params,
+  searchParams,
+}: {
+  params: { date: string };
+  searchParams?: { content?: string };
+}) {
+  const router = useRouter();
+  const { date } = params;
+  const { content } = searchParams ?? {};
+
+  const deleteText = (
+    <Box
+      color="text.text1"
+      cursor="pointer"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <TrashSolid size={24} />
+    </Box>
+  );
+
+  return (
+    <TopBarBottomButtonLayout
+      topbarTitle="편지함"
+      nextLabel="완료"
+      topbarRightContent={deleteText}
+      onNext={() => {
+        router.push(`/letters`);
+      }}
+    >
+      <Box display="flex" flexDirection="column" mt="20px">
+        <LetterEditBox defaultValue={content ?? ""} />
+      </Box>
+    </TopBarBottomButtonLayout>
+  );
+}
---
+++ src/app/letters/[date]/me/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { TopBarBottomButtonLayout } from "@/components/Layouts/TopBarBottomButtonLayout";
+import LetterCard from "@/components/Letter/LetterCard";
+import { Box } from "@chakra-ui/react";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+
+export default function MyLetterPage() {
+  const router = useRouter();
+  const { date } = useParams<{ date: string }>(); 
+  const sp = useSearchParams();
+
+  const userName = sp.get("userName") ?? "익명";
+  const content = sp.get("content") ?? "내용 없음";
+
+  return (
+    <TopBarBottomButtonLayout
+      topbarTitle="편지함"
+      nextLabel="수정하기"
+      onNext={() => {
+        router.push(
+          `/letters/${date}/me/edit?content=${encodeURIComponent(content)}`
+        );
+      }}
+    >
+      <Box display="flex" flexDirection="column" mt="20px">
+        <LetterCard userName={userName} date={String(date)} content={content} />
+      </Box>
+    </TopBarBottomButtonLayout>
+  );
+}

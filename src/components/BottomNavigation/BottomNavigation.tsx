5410fb9fad93f31e6939d15d3e15702d10932144
Chaejy <xhdrp@sookmyung.ac.kr>
2025-09-04 12:22:31 +0900

    polish: 다크모드 반영 수정

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -1,6 +1,6 @@
-/** @jsxImportSource @emotion/react */
-"use client"
-import { css } from '@emotion/react';
+"use client";
+
+import { Flex } from '@chakra-ui/react';
 import { usePathname } from 'next/navigation';
 import { BottomNavigationItem } from './BottomNavigationItem';
 import { CalendarSolid, BookOpenSolid, HomeSmileSolid, CenterFocusSolid, UserSquareSolid } from '@mynaui/icons-react';
@@ -9,43 +9,34 @@ export const BottomNavigation = () => {
   const pathname = usePathname();
 
   return (
-    <nav css={containerStyle}>
-      <div css={contentStyle}>
+    <Flex
+    position="fixed"
+    bottom="0"
+    left="0"
+    zIndex="1000"
+    w="100dvw"
+    borderRadius="1.25rem 1.25rem 0 0"
+    pb="10px"
+    bg="bg.bg3"
+    h="54px"
+    borderColor="border.border"
+    borderTop="0.5px solid"
+    borderRight="0.5px solid"
+    borderLeft="0.5px solid"
+    > 
+      <Flex
+      display="flex"
+      alignItems="center"
+      flexDirection="row"
+      justifyContent="center"
+      w="100%"
+      >
         <BottomNavigationItem icon={<CalendarSolid />} label="일정" href="/schedule" isActive={pathname === '/schedule'} />
         <BottomNavigationItem icon={<BookOpenSolid />} label="정보" href="/info" isActive={pathname === '/info'} />
-        <BottomNavigationItem icon={<HomeSmileSolid />} label="홈" href="/" isActive={pathname === '/'} />
+        <BottomNavigationItem icon={<HomeSmileSolid />} label="홈" href="/home" isActive={pathname === '/home'} />
         <BottomNavigationItem icon={<CenterFocusSolid />} label="스캔" href="/scan" isActive={pathname === '/scan'} />
         <BottomNavigationItem icon={<UserSquareSolid />} label="마이" href="/mypage" isActive={pathname === '/mypage'} />
-      </div>
-    </nav>
+      </Flex>
+    </Flex>
   );
 };
-
-const containerStyle = css`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  z-index: 1000;
-  width: 100%;
-  background: var(--BG-BG-3, #FFF);
-  border-top: 0.5px solid var(--Border-Border, #E8E7E7);
-  border-right: 0.5px solid var(--Border-Border, #E8E7E7);
-  border-left: 0.5px solid var(--Border-Border, #E8E7E7);
-  border-radius: 1.25rem 1.25rem 0 0;
-  padding-bottom: 10px;
-`;
-
-const contentStyle = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-
-  height: 44px; /* default: iOS 기준 */
-  height: 44px; /* default: iOS 기준 */
-
-  @media (max-width: 360px) {
-    height: 52px; /* AOS 기준 */
-    height: 52px; /* AOS 기준 */
-  }
-`;

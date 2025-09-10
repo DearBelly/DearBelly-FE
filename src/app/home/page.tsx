d5c15014693d8df222bb81f93e61f362317c871a
Chaejy <xhdrp@sookmyung.ac.kr>
2025-09-10 10:33:31 +0900

    feat: 상점 배경 변경 기능 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,9 +8,12 @@ import { Button } from "@/components/Button/Button";
 import ShaderBg from "@/components/Home/Background/ShaderBg";
 import { useRouter } from "next/navigation";
 import { ChakraIcons } from "@/utils/withChakraIcon";
+import { useBackgroundStore } from "@/store/useBackgroundStore";
 
 export default function Home() {
   const router = useRouter();
+  const bgState = useBackgroundStore();
+  const defaultUrl = "https://www.shadergradient.co/customize?animate=on&axesHelper=on&bgColor1=%23000000&bgColor2=%23000000&brightness=1.5&cAzimuthAngle=60&cDistance=7.1&cPolarAngle=90&cameraZoom=25&color1=%23ff7a33&color2=%2333a0ff&color3=%23ffc53d&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=50&frameRate=10&grain=off&http%3A%2F%2Flocalhost%3A3002%2Fcustomize%3Fanimate=on&lightType=3d&pixelDensity=1&positionX=0&positionY=-0.15&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=2&uDensity=1.1&uFrequency=5.5&uSpeed=0.05&uStrength=0.4&uTime=0&wireframe=false&zoomOut=false";
 
   const handleImageCardClick = (url: string) => {
     router.push(url);
@@ -33,7 +36,7 @@ export default function Home() {
     <Flex w="100%" justify="center">
       {/* 배경 */}
       <ShaderBg
-        urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=on&bgColor1=%23000000&bgColor2=%23000000&brightness=1.5&cAzimuthAngle=60&cDistance=7.1&cPolarAngle=90&cameraZoom=25&color1=%23ff7a33&color2=%2333a0ff&color3=%23ffc53d&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=50&frameRate=10&grain=off&http%3A%2F%2Flocalhost%3A3002%2Fcustomize%3Fanimate=on&lightType=3d&pixelDensity=1&positionX=0&positionY=-0.15&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=0&rotationY=0&rotationZ=0&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=2&uDensity=1.1&uFrequency=5.5&uSpeed=0.05&uStrength=0.4&uTime=0&wireframe=false&zoomOut=false"
+        urlString={bgState.backgrounds.find((bg) => bg.id === bgState.appliedId)?.urlPath || defaultUrl}
       />
       
       <Flex w="100%" maxW="40rem" m="0 auto" position="relative">
---
+++ src/app/store/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { TopBarBottomButtonLayout } from "@/components/Layouts/TopBarBottomButtonLayout";
+import { Box, Grid } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { useBackgroundStore } from "@/store/useBackgroundStore";
+import { ChakraIcons } from "@/utils/withChakraIcon";
+import Image from "next/image";
+
+export default function StorePage() {
+  const router = useRouter();
+  const bgState = useBackgroundStore();
+
+  const handleApply = () => {
+    bgState.apply();
+  };
+
+  return (
+    <TopBarBottomButtonLayout
+      topbarTitle="상점"
+      nextLabel="완료"
+      onNext={handleApply}
+      onBack={() => router.push("/home")}
+    >
+      <Grid mt="20px" templateColumns="repeat(2, 1fr)" gap="12px">
+        {bgState.backgrounds.map((bg) => {
+          const isApplied = bgState.isApplied(bg.id);
+          const isChecked = bgState.isChecked(bg.id);
+
+          return (
+            <Box
+              key={bg.id}
+              bgImage={bg.thumbnailPath}
+              bgSize="cover"
+              bgPos="center"
+              w="42.666dvw"
+              h="190px"
+              maxW="264px"
+              borderRadius="16px"
+              position="relative"
+              onClick={() => bgState.click(bg.id)}
+            >
+              <Box position="absolute" top="12px" left="12px">
+                {isApplied ? (
+                  <Image
+                    src="/images/icon_applied.svg"
+                    alt="applied"
+                    width={54}
+                    height={22}
+                  />
+                ) : (
+                  <ChakraIcons.CheckCircleSolid
+                    size={24}
+                    color={
+                      isChecked
+                        ? "icon.icon5" 
+                        : "rgba(255,255,255,0.2)" 
+                    }
+                  />
+                )}
+              </Box>
+            </Box>
+          );
+        })}
+      </Grid>
+    </TopBarBottomButtonLayout>
+  );
+}

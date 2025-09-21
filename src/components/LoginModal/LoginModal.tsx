80e0955551c353fff4e528dc2fa65bd2ea4719bb
chaejy <xjdrp@sookmyung.ac.kr>
2025-09-21 21:15:03 +0900

    feat: 로그인 모달 상태 제어 추가

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,66 +1,66 @@
 import { Box, Text } from '@chakra-ui/react';
-import { Button } from '@/components/Button'; 
+import { Button } from '@/components/Button';
 import { useRouter } from 'next/navigation';
 
-// export const LoginModal = ({ onClose }: { onClose: () => void }) => {
-export const LoginModal = () => {
-    const router = useRouter();
+interface LoginModalProps {
+  onClose: () => void;
+}
 
-    const handleLoginClick = () => {
-        router.push('/login');
-    };
+export const LoginModal = ({ onClose }: LoginModalProps) => {
+  const router = useRouter();
 
-    return (
-        <>
-        <Box
-            className='wrapper'
-            position="fixed"
-            top={0}
-            left={0}
-            w="100vw"
-            h="100vh"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            bg="rgba(0, 0, 0, 0.5)"  
-            zIndex={9999}
-        >
-            <Box
-                className='modal_wrapper'
-                position="relative"
-                borderRadius="1.25rem"
-                p="1.75rem 1rem 1rem 1rem"
-                bg="bg.bg3"
-                w="80%"
-                maxW="20rem"
-                transform="translateY(-50px)"  
-            >
-                <Box px="2rem" textAlign="center">
-                    <Text
-                        color="text.text1"
-                        textAlign="body_14700120"
-                        whiteSpace="normal"      
-                        overflow="visible"
-                        textOverflow="clip"
-                        wordBreak="keep-all"
-                    >
-                        로그인이 필요한 서비스입니다.
-                    </Text>
-                </Box>
+  const handleLoginClick = () => {
+    router.push('/login');
+  };
 
-                <Box
-                    display="flex"
-                    gap="0.5rem"
-                    justifyContent="center"
-                    mt="1.5rem"
-                    px="0.5rem"
-                >
-                    <Button type="primary" size="medium" width="100%" onClick={handleLoginClick}>
-                        확인
-                    </Button>
-                </Box>
-            </Box>
+  return (
+    <Box
+      className="wrapper"
+      position="fixed"
+      top={0}
+      left={0}
+      w="100vw"
+      h="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      bg="rgba(0, 0, 0, 0.5)"
+      zIndex={9999}
+      onClick={onClose} 
+    >
+      <Box
+        className="modal_wrapper"
+        position="relative"
+        borderRadius="1.25rem"
+        p="1.75rem 1rem 1rem 1rem"
+        bg="bg.bg3"
+        maxW="20rem"
+        w="100%"
+        transform="translateY(-50px)"
+        onClick={(e) => e.stopPropagation()} 
+      >
+        <Box w="100%" textAlign="center">
+          <Text color="text.text1" textStyle="body_168001">
+            로그인이 필요한 서비스입니다.
+          </Text>
         </Box>
-        </>
-    );
+
+        <Box display="flex" justifyContent="center" mt="1.75rem">
+          <Button
+            type="primary"
+            size="medium"
+            width="100%"
+            onClick={handleLoginClick}
+          >
+            <Text
+              textStyle="caption_12800"
+              color="button.button.text.secondary"
+            >
+              로그인하러 가기
+            </Text>
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 };

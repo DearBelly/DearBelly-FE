4aa8ed9fa504c5b59332be2e14e900762f670f5d
Jiyeon <144954836+Chaejy@users.noreply.github.com>
2025-09-22 09:30:53 +0900

    [FEAT] 사이트 세팅 (#97)
    * fix: 마이페이지 conflict 수정
    
    * feat: 홈 추천 정보 api 연동
    
    * fix: 카테고리 선택 유틸 수정
    
    * fix: 회원가입 시 상태관리 변수명 조정
    
    * fix: 회원가입 스텝 마지막에 통신하도록 수정
    
    * feat: 마이페이지 아기 정보 등록
    
    * feat: 소셜 로그인 리다이렉트 설정
    
    * chore: 오타 수정
    
    * feat: 배너 및 캐릭터 변경 로직 추가
    
    * fix: 빌드 에러 수정
    
    * chore: 오타 수정
    
    * feat: 카카오 스크립트 분리
    
    * feat: 서버 컴포넌트로 변경 및 파비콘 추가
    
    * fix: 사파리 하단 잘림 대응
    
    ---------
    
    Co-authored-by: chaejy <xjdrp@sookmyung.ac.kr>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,35 @@
-'use client';
 import { Provider } from "@/components/UI/Provider"
 import "@/app/globals.css"
-import Script from "next/script"; 
+import { KakaoScript } from "@/components/KakaoScript/KakaoScript";
+import { Metadata } from "next";
+import { Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "Dear Belly",
+  description: "엄마를 위한 케어, 아이를 위한 기록",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" }
+    ],
+    apple: [
+      { url: "/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#f9f7f7" },
+    { media: "(prefers-color-scheme: dark)", color: "#202020" },
+  ],
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
+        <link rel="favicon" href="./favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
       {/* <Script
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.6/kakao.min.js"
         integrity={process.env.NEXT_PUBLIC_KAKAO_SDK_INTEGRITY}
@@ -21,15 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body>
         <Provider>{children}</Provider>
-        <Script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.6/kakao.min.js"
-          strategy="afterInteractive"
-          onLoad={() => {
-            if (window.Kakao && !window.Kakao.isInitialized()) {
-              window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
-            }
-          }}
-        />
+        <KakaoScript />
       </body>
     </html>
   )
--- src/components/UI/Provider.tsx
+++ src/components/UI/Provider.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import '../../app/globals.css'; 
 import { ChakraProvider } from "@chakra-ui/react"
 import { ThemeProvider } from "next-themes"

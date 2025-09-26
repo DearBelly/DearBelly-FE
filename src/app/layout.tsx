259f219ab37cec22730fe3769202788bd32c9608
Chaejy <xhdrp@sookmyung.ac.kr>
2025-09-27 02:38:45 +0900

    feat: 로그인 auth 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,19 +28,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
-        <link rel="favicon" href="./favicon-32x32.png" />
-        <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
-      {/* <Script
-        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.6/kakao.min.js"
-        integrity={process.env.NEXT_PUBLIC_KAKAO_SDK_INTEGRITY}
-        crossOrigin="anonymous"
-        strategy="beforeInteractive"
-        onLoad={() => {
-          if (window.Kakao && !window.Kakao.isInitialized()) {
-            window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
-          }
-        }}
-      /> */}
       </head>
       <body>
         <Provider>{children}</Provider>
